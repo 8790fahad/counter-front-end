67d84ff830e501aa9e2511f72ada4a058804366a
8790fahad <8790fahadado@gmail.com>
2024-10-29 15:27:43 +0100

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  base: '/first_contract_front_end/',
+  base: '/counter-front-end/',
   build: {
     chunkSizeWarningLimit: 2000,
   },
